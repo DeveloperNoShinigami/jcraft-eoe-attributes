3c1f81c8c6cd371e0445b61ca0a34f38af4f0154
ayutac <fly.high.android@gmail.com>
2024-05-08 17:17:16 +0200

    More datagen.

--- fabric/src/main/java/net/arna/jcraft/fabric/datagen/JModelProvider.java
+++ fabric/src/main/java/net/arna/jcraft/fabric/datagen/JModelProvider.java
@@ -15,13 +15,40 @@ public class JModelProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator generator) {
+        generator.registerSimpleCubeAll(JBlockRegistry.FOOLISH_SAND_BLOCK.get());
         generator.registerSimpleCubeAll(JBlockRegistry.METEORITE_BLOCK.get());
         generator.registerSimpleCubeAll(JBlockRegistry.METEORITE_IRON_ORE_BLOCK.get());
+        generator.registerSimpleCubeAll(JBlockRegistry.SOUL_BLOCK.get());
     }
 
     @Override
     public void generateItemModels(ItemModelGenerator generator) {
+        generator.register(JItemRegistry.BOXINGGLOVES.get(), Models.GENERATED);
+        generator.register(JItemRegistry.BULLET.get(), Models.GENERATED);
+        generator.register(JItemRegistry.CINDERELLA_MASK.get(), Models.GENERATED);
+        generator.register(JItemRegistry.DIOBOOTS.get(), Models.GENERATED);
+        generator.register(JItemRegistry.DIOCAPE.get(), Models.GENERATED);
+        generator.register(JItemRegistry.DIOHEADBAND.get(), Models.GENERATED);
+        generator.register(JItemRegistry.DIOJACKET.get(), Models.GENERATED);
+        generator.register(JItemRegistry.DIOPANTS.get(), Models.GENERATED);
+        generator.register(JItemRegistry.DIOSDIARY.get(), Models.GENERATED);
+        generator.register(JItemRegistry.GREENBABY.get(), Models.GENERATED);
+        generator.register(JItemRegistry.JOTAROBOOTS.get(), Models.GENERATED);
+        generator.register(JItemRegistry.JOTAROCAP.get(), Models.GENERATED);
+        generator.register(JItemRegistry.JOTAROJACKET.get(), Models.GENERATED);
+        generator.register(JItemRegistry.JOTAROPANTS.get(), Models.GENERATED);
+        generator.register(JItemRegistry.KARSHEADWRAP.get(), Models.GENERATED);
+        generator.register(JItemRegistry.KNIFE.get(), Models.HANDHELD);
+        generator.register(JItemRegistry.KQ_COIN.get(), Models.GENERATED);
+        generator.register(JItemRegistry.LIVINGARROW.get(), Models.HANDHELD);
+        generator.register(JItemRegistry.RED_HAT.get(), Models.GENERATED);
+        generator.register(JItemRegistry.REQUIEMARROW.get(), Models.GENERATED);
+        generator.register(JItemRegistry.REQUIEMRUBY.get(), Models.HANDHELD);
+        generator.register(JItemRegistry.SINNERSSOUL.get(), Models.GENERATED);
+        generator.register(JItemRegistry.STANDARROW.get(), Models.HANDHELD);
         generator.register(JItemRegistry.STAND_ARROWHEAD.get(), Models.GENERATED);
+        generator.register(JItemRegistry.STAND_DISC.get(), Models.GENERATED);
         generator.register(JItemRegistry.STELLAR_IRON_INGOT.get(), Models.GENERATED);
+        generator.register(JItemRegistry.STONE_MASK.get(), Models.GENERATED);
     }
 }
