946011623dcff73803055d9cb4d82365613d7ff0
ayutac <fly.high.android@gmail.com>
2024-05-12 15:11:15 +0200

    Added stellar iron trim.

--- fabric/src/main/java/net/arna/jcraft/fabric/datagen/JTagProviders.java
+++ fabric/src/main/java/net/arna/jcraft/fabric/datagen/JTagProviders.java
@@ -46,6 +46,8 @@ public class JTagProviders {
 
         @Override
         protected void configure(RegistryWrapper.WrapperLookup arg) {
+            getTagBuilder(ItemTags.TRIM_MATERIALS).add(JItemRegistry.STELLAR_IRON_INGOT.getId());
+
             getTagBuilder(ItemTags.SAND).add(JItemRegistry.HOT_SAND_BLOCK.getId());
             getTagBuilder(ItemTags.SMELTS_TO_GLASS).add(JItemRegistry.HOT_SAND_BLOCK.getId());
 
