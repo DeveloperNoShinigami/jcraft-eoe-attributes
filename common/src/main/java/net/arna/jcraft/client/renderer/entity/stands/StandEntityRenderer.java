e98a15318692dc85465f9f49bd315159a91fa1a7
mrsterner <simon@sterner.dev>
2024-05-14 19:05:54 +0200

    fixed some geo stand rendering, they still spin??

--- common/src/main/java/net/arna/jcraft/client/renderer/entity/stands/StandEntityRenderer.java
+++ common/src/main/java/net/arna/jcraft/client/renderer/entity/stands/StandEntityRenderer.java
@@ -1,32 +1,31 @@
 package net.arna.jcraft.client.renderer.entity.stands;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.arna.jcraft.common.entity.stand.StandEntity;
 import net.arna.jcraft.common.util.JUtils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.VertexConsumers;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.*;
 import net.minecraft.world.LightType;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix4f;
+import org.joml.Quaternionf;
 import software.bernie.geckolib.cache.object.BakedGeoModel;
+import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.constant.DataTickets;
 import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.core.object.Color;
 import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.model.data.EntityModelData;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.GeoRenderer;
 
 public class StandEntityRenderer<T extends StandEntity<?, ?>> extends GeoEntityRenderer<T> {
 
@@ -81,13 +80,7 @@ public class StandEntityRenderer<T extends StandEntity<?, ?>> extends GeoEntityR
 
     @Override
     public void actuallyRender(MatrixStack poseStack, T animatable, BakedGeoModel model, RenderLayer renderType, VertexConsumerProvider bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-
         poseStack.push();
-
-        Entity leashHolder = animatable.getHoldingEntity();
-        if (leashHolder != null) {
-            renderLeash(animatable, partialTick, poseStack, bufferSource, leashHolder);
-        }
 
         boolean shouldSit = animatable.hasVehicle() && (animatable.getVehicle() != null);
         float lerpBodyRot = MathHelper.lerpAngleDegrees(partialTick, animatable.prevBodyYaw, animatable.bodyYaw);
@@ -100,11 +93,20 @@ public class StandEntityRenderer<T extends StandEntity<?, ?>> extends GeoEntityR
             float clampedHeadYaw = MathHelper.clamp(MathHelper.wrapDegrees(netHeadYaw), -85, 85);
             lerpBodyRot = lerpHeadRot - clampedHeadYaw;
 
-            if (clampedHeadYaw * clampedHeadYaw > 2500f) {
+            if (clampedHeadYaw * clampedHeadYaw > 2500f)
                 lerpBodyRot += clampedHeadYaw * 0.2f;
-            }
 
             netHeadYaw = lerpHeadRot - lerpBodyRot;
+        }
+
+        if (animatable.getPose() == EntityPose.SLEEPING && animatable != null) {
+            Direction bedDirection = animatable.getSleepingDirection();
+
+            if (bedDirection != null) {
+                float eyePosOffset = animatable.getEyeHeight(EntityPose.STANDING) - 0.1F;
+
+                poseStack.translate(-bedDirection.getOffsetX() * eyePosOffset, 0, -bedDirection.getOffsetZ() * eyePosOffset);
+            }
         }
 
         float ageInTicks = animatable.age + partialTick;
@@ -113,62 +115,62 @@ public class StandEntityRenderer<T extends StandEntity<?, ?>> extends GeoEntityR
 
         applyRotations(animatable, poseStack, ageInTicks, lerpBodyRot, partialTick);
 
-        AnimationState<T> predicate = new AnimationState<T>(animatable, limbSwing, limbSwingAmount, partialTick,
-                (limbSwingAmount <= -getSwingMotionAnimThreshold() || limbSwingAmount > getSwingMotionAnimThreshold()));
+        if (!shouldSit && animatable.isAlive()) {
+            limbSwingAmount = animatable.limbAnimator.getSpeed(partialTick);
+            limbSwing = animatable.limbAnimator.getPos(partialTick);
 
+            if (animatable.isBaby())
+                limbSwing *= 3f;
 
-        this.model.setCustomAnimations(animatable, getInstanceId(animatable), predicate);
-
-        this.modelRenderTranslations = new Matrix4f(poseStack.peek().getPositionMatrix());
+            if (limbSwingAmount > 1f)
+                limbSwingAmount = 1f;
+        }
 
         if (!isReRender) {
             float headPitch = MathHelper.lerp(partialTick, animatable.prevPitch, animatable.getPitch());
             float motionThreshold = getMotionAnimThreshold(animatable);
             Vec3d velocity = animatable.getVelocity();
-            float avgVelocity = (float) (Math.abs(velocity.x) + Math.abs(velocity.z) / 2f);
+            float avgVelocity = (float)(Math.abs(velocity.x) + Math.abs(velocity.z) / 2f);
             AnimationState<T> animationState = new AnimationState<T>(animatable, limbSwing, limbSwingAmount, partialTick, avgVelocity >= motionThreshold && limbSwingAmount != 0);
             long instanceId = getInstanceId(animatable);
 
             animationState.setData(DataTickets.TICK, animatable.getTick(animatable));
             animationState.setData(DataTickets.ENTITY, animatable);
-            animationState.setData(DataTickets.ENTITY_MODEL_DATA, new EntityModelData(shouldSit, animatable != null && animatable.isBaby(), -netHeadYaw, -headPitch));
+            animationState.setData(DataTickets.ENTITY_MODEL_DATA, new EntityModelData(shouldSit, animatable.isBaby(), -netHeadYaw, -headPitch));
             this.model.addAdditionalStateData(animatable, instanceId, animationState::setData);
             this.model.handleAnimations(animatable, instanceId, animationState);
         }
 
         poseStack.translate(0, 0.01f, 0);
-        RenderSystem.setShaderTexture(0, getTextureLocation(animatable));
 
-        Color renderColor = getRenderColor(animatable, partialTick, packedLight);
+        this.modelRenderTranslations = new Matrix4f(poseStack.peek().getPositionMatrix());
 
-        if (!animatable.isInvisibleTo(MinecraftClient.getInstance().player)) {
-            VertexConsumer glintBuffer = bufferSource.getBuffer(RenderLayer.getDirectEntityGlint());
-            VertexConsumer translucentBuffer = bufferSource.getBuffer(RenderLayer.getEntityTranslucentCull(getTextureLocation(animatable)));
+        if (animatable.isInvisibleTo(MinecraftClient.getInstance().player)) {
+            if (MinecraftClient.getInstance().hasOutline(animatable)) {
+                buffer = bufferSource.getBuffer(renderType = RenderLayer.getOutline(getTexture(animatable)));
+            }
+            else {
+                renderType = null;
+            }
+        }
 
-            VertexConsumer union = VertexConsumers.union(glintBuffer, translucentBuffer);
+        if (renderType != null) {
+            updateAnimatedTextureFrame(animatable);
 
-            super.actuallyRender(
-                    poseStack,
-                    animatable,
-                    model,
-                    renderType,
-                    bufferSource,
-                    glintBuffer != translucentBuffer ? union : null,
-                    false,
-                    partialTick,
-                    packedLight,
-                    getPackedOverlay(animatable, 0),
-                    renderColor.getRed() / 255f,
-                    renderColor.getGreen() / 255f,
-                    renderColor.getBlue() / 255f,
-                    renderColor.getAlpha() / 255f);
+            Color renderColor = getRenderColor(animatable, partialTick, packedLight);
+
+            for (GeoBone group : model.topLevelBones()) {
+                renderRecursively(poseStack, animatable, group, renderType, bufferSource, buffer, isReRender, partialTick, packedLight,
+                        packedOverlay,
+                        renderColor.getRed() / 255f,
+                        renderColor.getGreen() / 255f,
+                        renderColor.getBlue() / 255f,
+                        renderColor.getAlpha() / 255f
+                );
+            }
         }
 
         poseStack.pop();
-    }
-
-    protected float getSwingMotionAnimThreshold() {
-        return 0.15f;
     }
 
     @Override
