940d2c05bbd232312472ad458bbbb3447244b099
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2024-05-14 17:24:22 +0200

    Fix load order issue with TerraBlender

--- fabric/src/main/java/net/arna/jcraft/fabric/JCraftFabric.java
+++ fabric/src/main/java/net/arna/jcraft/fabric/JCraftFabric.java
@@ -5,6 +5,7 @@ import net.arna.jcraft.common.argumenttype.AttackArgumentType;
 import net.arna.jcraft.common.argumenttype.SpecArgumentType;
 import net.arna.jcraft.common.argumenttype.StandArgumentType;
 import net.arna.jcraft.common.events.JServerEvents;
+import net.arna.jcraft.fabric.common.terrablender.JTerraFabric;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.ArgumentTypeRegistry;
 import net.fabricmc.fabric.api.entity.event.v1.EntitySleepEvents;
@@ -21,6 +22,7 @@ public final class JCraftFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         JCraft.init();
+        JTerraFabric.onModInitialized();
 
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
             for (Consumer<LootTable.Builder> modification : modifications.get(id)) modification.accept(tableBuilder);
--- fabric/src/main/java/net/arna/jcraft/fabric/common/terrablender/JTerraFabric.java
+++ fabric/src/main/java/net/arna/jcraft/fabric/common/terrablender/JTerraFabric.java
@@ -6,10 +6,24 @@ import terrablender.api.SurfaceRuleManager;
 import terrablender.api.TerraBlenderApi;
 
 public class JTerraFabric implements TerraBlenderApi {
+    private static boolean initialized = false;
+    private static boolean modInitialized = false;
 
     @Override
     public void onTerraBlenderInitialized() {
         Regions.register(new OverworldRegionFabric(JCraft.id("overworld"), 4));
+        initialized = true;
+        if (modInitialized)
+            registerSurfaceRules();
+    }
+
+    public static void onModInitialized() {
+        modInitialized = true;
+        if (initialized)
+            registerSurfaceRules();
+    }
+
+    private static void registerSurfaceRules() {
         SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, JCraft.MOD_ID, MaterialRulesFabric.makeRules());
     }
 }
