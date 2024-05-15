a6b881e120a924d23fbd76f11f7c7ecc1dd6d5d7
mrsterner <simon@sterner.dev>
2024-05-15 17:33:05 +0200

    petshop up my balls

--- fabric/src/main/java/net/arna/jcraft/fabric/datagen/JLootTableProviders.java
+++ fabric/src/main/java/net/arna/jcraft/fabric/datagen/JLootTableProviders.java
@@ -1,22 +1,31 @@
 package net.arna.jcraft.fabric.datagen;
 
+import com.google.common.collect.Maps;
 import net.arna.jcraft.JCraft;
 import net.arna.jcraft.registry.JBlockRegistry;
+import net.arna.jcraft.registry.JEntityTypeRegistry;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.SimpleFabricLootTableProvider;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
+import net.minecraft.loot.LootTables;
 import net.minecraft.loot.context.LootContextTypes;
 import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootTableEntry;
 import net.minecraft.loot.function.LootFunction;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
 
+import java.util.Map;
 import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 public class JLootTableProviders {
 
@@ -38,18 +47,31 @@ public class JLootTableProviders {
 
     public static class EntityLoot extends SimpleFabricLootTableProvider {
 
+        private final Map<Identifier, LootTable.Builder> loot = Maps.newHashMap();
         public EntityLoot(FabricDataOutput output) {
             super(output, LootContextTypes.ENTITY);
         }
 
         @Override
         public void accept(BiConsumer<Identifier, LootTable.Builder> consumer) {
-            consumer.accept(JCraft.id("petshop"), LootTable.builder()
-                    .pool(constantPool(1f)
-                            .with(ItemEntry.builder(Items.FEATHER).apply(uniformAmount(1f, 2f))))
-                    .pool(constantPool(1f)
-                            .with(ItemEntry.builder(Items.CHAIN).apply(constantAmount(1f))))
-            );
+            this.generateLoot();
+            for (Map.Entry<Identifier, LootTable.Builder> entry : loot.entrySet()) {
+                consumer.accept(entry.getKey(), entry.getValue());
+            }
+        }
+
+        private void generateLoot() {
+            addDrop(JEntityTypeRegistry.PETSHOP.get(), this::petshopLoot);
+        }
+
+        public <T extends Entity> void addDrop(EntityType<T> type, Function<EntityType<T>, LootTable.Builder> function) {
+            loot.put(type.getLootTableId(), function.apply(type));
+        }
+
+        private LootTable.Builder petshopLoot(EntityType<?> type) {
+            return LootTable.builder()
+                    .pool(constantPool(1f).with(ItemEntry.builder(Items.FEATHER).apply(uniformAmount(1f, 2f))))
+                    .pool(constantPool(1f).with(ItemEntry.builder(Items.CHAIN).apply(constantAmount(1f))));
         }
     }
 
