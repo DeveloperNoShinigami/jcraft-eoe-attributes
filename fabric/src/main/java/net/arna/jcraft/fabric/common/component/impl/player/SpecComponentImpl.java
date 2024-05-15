4c09809bb913fd0f6cecf721fa360ec6a7c8559b
mrsterner <simon@sterner.dev>
2024-05-15 22:22:20 +0200

    dont crash silly fabric

--- fabric/src/main/java/net/arna/jcraft/fabric/common/component/impl/player/SpecComponentImpl.java
+++ fabric/src/main/java/net/arna/jcraft/fabric/common/component/impl/player/SpecComponentImpl.java
@@ -4,6 +4,7 @@ import lombok.NonNull;
 import net.arna.jcraft.common.component.impl.player.CommonSpecComponentImpl;
 import net.arna.jcraft.fabric.common.component.JComponents;
 import net.arna.jcraft.fabric.common.component.player.SpecComponent;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -17,7 +18,7 @@ public class SpecComponentImpl extends CommonSpecComponentImpl implements SpecCo
     }
 
     @Override
-    public void sync() {
+    public void sync(Entity entity) {
         JComponents.SPEC.sync(player);
     }
 
