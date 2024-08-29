0cd33bd98e86bccce2421f885982ed161a9ed07d
ayutac <fly.high.android@gmail.com>
2024-08-29 15:09:48 +0200

    Coffin functionality done on forge.

--- forge/src/main/java/net/arna/jcraft/forge/events/RuntimeEvents.java
+++ forge/src/main/java/net/arna/jcraft/forge/events/RuntimeEvents.java
@@ -1,6 +1,7 @@
 package net.arna.jcraft.forge.events;
 
 import net.arna.jcraft.JCraft;
+import net.arna.jcraft.common.block.tile.CoffinTileEntity;
 import net.arna.jcraft.forge.capability.api.JCapabilityProvider;
 import net.arna.jcraft.forge.capability.impl.entity.GrabCapability;
 import net.arna.jcraft.forge.capability.impl.entity.TimeStopCapability;
@@ -8,16 +9,22 @@ import net.arna.jcraft.forge.capability.impl.living.*;
 import net.arna.jcraft.forge.capability.impl.player.PhCapability;
 import net.arna.jcraft.forge.capability.impl.player.SpecCapability;
 import net.arna.jcraft.forge.capability.impl.world.ShockwaveHandlerCapability;
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.entity.player.SleepingTimeCheckEvent;
+import net.minecraftforge.event.level.SleepFinishedTimeEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -82,5 +89,30 @@ public class RuntimeEvents {
     public static void playerClone(PlayerEvent.Clone event) {
         PhCapability.getCapabilityOptional(event.getOriginal()).ifPresent(o -> PhCapability.getCapabilityOptional(event.getEntity()).ifPresent(c -> c.deserializeNBT(o.serializeNBT())));
         SpecCapability.getCapabilityOptional(event.getOriginal()).ifPresent(o -> SpecCapability.getCapabilityOptional(event.getEntity()).ifPresent(c -> c.deserializeNBT(o.serializeNBT())));
+    }
+
+    @SubscribeEvent
+    public static void coffinSleepCheck(SleepingTimeCheckEvent event) {
+        Level world = event.getEntity().level();
+        if (event.getSleepingLocation().isPresent()) {
+            BlockPos pos = event.getSleepingLocation().get();
+            BlockEntity bed = world.getBlockEntity(pos);
+            if (bed instanceof CoffinTileEntity) {
+                if (world.isDay()) {
+                    event.setResult(Event.Result.ALLOW);
+                }
+                else {
+                    event.setResult(Event.Result.DENY);
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void coffinWakeUpTime(SleepFinishedTimeEvent event) {
+        LevelAccessor world = event.getLevel();
+        if (world.dayTime() % 24_000 < 12_000) { // before dusk
+            event.setTimeAddition((world.dayTime() / 24_000) * 24_000 + 13_000); // nighttime
+        }
     }
 }
