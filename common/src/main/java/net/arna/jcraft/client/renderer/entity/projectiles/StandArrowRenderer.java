e23ac238b5b31dcb78e4fa3a65d250abd13708f7
ayutac <fly.high.android@gmail.com>
2024-10-09 18:20:38 +0200

    Removed unused import.

--- common/src/main/java/net/arna/jcraft/client/renderer/entity/projectiles/StandArrowRenderer.java
+++ common/src/main/java/net/arna/jcraft/client/renderer/entity/projectiles/StandArrowRenderer.java
@@ -1,6 +1,5 @@
 package net.arna.jcraft.client.renderer.entity.projectiles;
 
-import mod.azure.azurelib.renderer.GeoEntityRenderer;
 import net.arna.jcraft.client.model.entity.StandArrowModel;
 import net.arna.jcraft.common.entity.projectile.StandArrowEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
