7c948f66fa7c4f94ab86ab9e0c761f9757ef394b
ayutac <fly.high.android@gmail.com>
2024-10-09 18:48:24 +0200

    Assigned stand on hit.

--- common/src/main/java/net/arna/jcraft/common/entity/projectile/StandArrowEntity.java
+++ common/src/main/java/net/arna/jcraft/common/entity/projectile/StandArrowEntity.java
@@ -5,12 +5,19 @@ import mod.azure.azurelib.animatable.GeoEntity;
 import mod.azure.azurelib.core.animatable.instance.AnimatableInstanceCache;
 import mod.azure.azurelib.core.animation.AnimatableManager;
 import mod.azure.azurelib.util.AzureLibUtil;
+import net.arna.jcraft.JCraft;
+import net.arna.jcraft.common.component.living.CommonStandComponent;
+import net.arna.jcraft.common.entity.stand.StandType;
+import net.arna.jcraft.common.util.JUtils;
+import net.arna.jcraft.platform.JComponentPlatformUtils;
 import net.arna.jcraft.registry.JEntityTypeRegistry;
 import net.arna.jcraft.registry.JItemRegistry;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
 
 public class StandArrowEntity extends AbstractArrow implements GeoEntity {
 
@@ -37,5 +44,21 @@ public class StandArrowEntity extends AbstractArrow implements GeoEntity {
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return geoCache;
+    }
+
+    @Override
+    protected void onHitEntity(final @NonNull EntityHitResult result) {
+        super.onHitEntity(result);
+        if (result.getEntity() instanceof final LivingEntity mob) {
+            if (JUtils.getStand(mob) == null) {
+                final CommonStandComponent standData = JComponentPlatformUtils.getStandData(mob);
+                standData.setType(StandType.getRandomRegular(random));
+                mob.unRide();
+                JCraft.summon(mob.level(), mob);
+            }
+            else if (mob instanceof final Player player) {
+                player.addItem(new ItemStack(JItemRegistry.STAND_ARROW.get()));
+            }
+        }
     }
 }
