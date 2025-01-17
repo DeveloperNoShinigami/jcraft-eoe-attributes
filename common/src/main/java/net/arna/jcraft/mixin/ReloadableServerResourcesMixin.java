9024149d4af4349cee54ab96216e81d2635fb58c
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2025-01-17 19:08:33 +0100

    Fix weird schizo missing movesets bug

--- common/src/main/java/net/arna/jcraft/mixin/ReloadableServerResourcesMixin.java
+++ common/src/main/java/net/arna/jcraft/mixin/ReloadableServerResourcesMixin.java
@@ -1,22 +1,21 @@
 package net.arna.jcraft.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.arna.jcraft.common.attack.core.data.MoveSetLoader;
 import net.minecraft.server.ReloadableServerResources;
 import net.minecraft.server.packs.resources.PreparableReloadListener;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Mixin(ReloadableServerResources.class)
+@Mixin(value = ReloadableServerResources.class, priority = 100)
 public class ReloadableServerResourcesMixin {
-    @Inject(method = "listeners", at = @At("RETURN"), cancellable = true)
-    private void addMoveSetLoaderToListeners(CallbackInfoReturnable<List<PreparableReloadListener>> cir) {
-        List<PreparableReloadListener> listeners = new ArrayList<>(cir.getReturnValue());
+    @ModifyReturnValue(method = "listeners", at = @At("RETURN"))
+    private List<PreparableReloadListener> addMoveSetLoaderToListeners(final List<PreparableReloadListener> original) {
+        List<PreparableReloadListener> listeners = new ArrayList<>(original);
         listeners.add(MoveSetLoader::onReload);
-        cir.setReturnValue(listeners);
+        return listeners;
     }
 }
