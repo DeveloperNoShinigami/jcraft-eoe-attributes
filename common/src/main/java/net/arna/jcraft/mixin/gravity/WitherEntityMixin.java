48860c1b69f9147ecb1e8c4f0ebe6fd304ace2db
Vinyl01 <rec0rd9874@gmail.com>
2025-03-12 23:27:57 -0500

    fixed wither aiming bug

--- common/src/main/java/net/arna/jcraft/mixin/gravity/WitherEntityMixin.java
+++ common/src/main/java/net/arna/jcraft/mixin/gravity/WitherEntityMixin.java
@@ -40,7 +40,7 @@ public abstract class WitherEntityMixin {
     private double redirect_shootSkullAt_getY_0(LivingEntity target) {
         Direction gravityDirection = GravityChangerAPI.getGravityDirection(target);
         if (gravityDirection == Direction.DOWN) {
-            return target.getX();
+            return target.getY();
         }
 
         return target.position().add(RotationUtil.vecPlayerToWorld(0.0D, target.getEyeHeight() * 0.5D, 0.0D, gravityDirection)).y - target.getEyeHeight() * 0.5D;
@@ -57,7 +57,7 @@ public abstract class WitherEntityMixin {
     private double redirect_shootSkullAt_getZ_0(LivingEntity target) {
         Direction gravityDirection = GravityChangerAPI.getGravityDirection(target);
         if (gravityDirection == Direction.DOWN) {
-            return target.getX();
+            return target.getZ();
         }
 
         return target.position().add(RotationUtil.vecPlayerToWorld(0.0D, target.getEyeHeight() * 0.5D, 0.0D, gravityDirection)).z;
