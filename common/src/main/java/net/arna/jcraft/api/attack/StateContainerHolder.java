63882c97404f40acd55f246b3b48d2402d47d54c
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2025-06-04 11:59:26 +0200

    Fix typo

--- common/src/main/java/net/arna/jcraft/api/attack/StateContainerHolder.java
+++ common/src/main/java/net/arna/jcraft/api/attack/StateContainerHolder.java
@@ -2,7 +2,7 @@ package net.arna.jcraft.api.attack;
 
 /**
  * Any class that holds state containers should implement this interface.
- * The {@link #configureStateContainers(Class)} is automatically called when the move implementing this is loaded.
+ * The {@link #configureStateContainers(Class)} method is automatically called when the move implementing this is loaded.
  * @param <S>
  */
 public interface StateContainerHolder<S extends Enum<S>> {
