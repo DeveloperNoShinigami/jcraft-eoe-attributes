43655730a17baae09b552891f6eb3efe466abada
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2025-06-07 14:14:46 +0200

    Implement new parsing method for arg types

--- common/src/main/java/net/arna/jcraft/api/JRegistries.java
+++ common/src/main/java/net/arna/jcraft/api/JRegistries.java
@@ -1,10 +1,14 @@
 package net.arna.jcraft.api;
 
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import dev.architectury.registry.registries.Registrar;
 import dev.architectury.registry.registries.RegistrarManager;
 import net.arna.jcraft.JCraft;
+import net.arna.jcraft.api.spec.SpecType2;
 import net.arna.jcraft.api.stand.StandType;
 import net.arna.jcraft.common.attack.core.MoveAction;
 import net.arna.jcraft.common.attack.core.MoveCondition;
@@ -15,6 +19,9 @@ import net.arna.jcraft.common.attack.moves.base.AbstractMove;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Simple class that holds the registries used by JCraft.
@@ -27,6 +34,9 @@ public class JRegistries {
 
     // Registries
     public static final Registrar<StandType> STAND_TYPE_REGISTRY = manager.<StandType>builder(JCraft.id("stand_type"))
+            .syncToClients()
+            .build();
+    public static final Registrar<SpecType2> SPEC_TYPE_REGISTRY = manager.<SpecType2>builder(JCraft.id("spec_type"))
             .syncToClients()
             .build();
     public static final Registrar<MoveType<?>> MOVE_TYPE_REGISTRY = manager.<MoveType<?>>builder(JCraft.id("move_type"))
@@ -42,11 +52,14 @@ public class JRegistries {
 
     // Registry keys
     public static final ResourceKey<Registry<StandType>> STAND_TYPE_REGISTRY_KEY = createKey(STAND_TYPE_REGISTRY);
+    public static final ResourceKey<Registry<SpecType2>> SPEC_TYPE_REGISTRY_KEY = createKey(SPEC_TYPE_REGISTRY);
     public static final ResourceKey<Registry<MoveType<?>>> MOVE_TYPE_REGISTRY_KEY = createKey(MOVE_TYPE_REGISTRY);
     public static final ResourceKey<Registry<MoveConditionType<?>>> MOVE_CONDITION_TYPE_REGISTRY_KEY = createKey(MOVE_CONDITION_TYPE_REGISTRY);
     public static final ResourceKey<Registry<MoveActionType<?>>> MOVE_ACTION_TYPE_REGISTRY_KEY = createKey(MOVE_ACTION_TYPE_REGISTRY);
 
     // Registry codecs
+    public static final Codec<StandType> STAND_TYPE_CODEC = createCodec(STAND_TYPE_REGISTRY);
+    public static final Codec<SpecType2> SPEC_TYPE_CODEC = createCodec(SPEC_TYPE_REGISTRY);
     public static final Codec<MoveType<?>> MOVE_TYPE_CODEC = createCodec(MOVE_TYPE_REGISTRY);
     public static final Codec<AbstractMove<?, ?>> MOVE_CODEC = MOVE_TYPE_CODEC
             .dispatch("type", AbstractMove::getMoveType, MoveType::getCodec);
@@ -82,5 +95,49 @@ public class JRegistries {
     // something that extends Registry<T>, but not Registry<T> itself, which is problematic.
     private static <T> ResourceKey<Registry<T>> createKey(Registrar<T> registrar) {
         return ResourceKey.createRegistryKey(registrar.key().location());
+    }
+
+    /**
+     * Utility method to parse a registry entry from a StringReader.
+     * @param registry the registry to parse from
+     * @param reader the StringReader to parse from
+     * @return the parsed registry entry, or null if not found
+     * @param <T> the type of the registry entry
+     */
+    public static <T> @Nullable T parseRegistryEntry(Registrar<T> registry, StringReader reader) {
+        int i = reader.getCursor();
+        while (reader.canRead() && ResourceLocation.isAllowedInResourceLocation(reader.peek())) {
+            reader.skip();
+        }
+        final String string = reader.getString().substring(i, reader.getCursor());
+
+        String namespace = JCraft.MOD_ID;
+        String path = string;
+        if (string.contains(":")) {
+            final String[] parts = string.split(":", 2);
+            namespace = parts[0];
+            path = parts[1];
+        }
+
+        ResourceLocation id = new ResourceLocation(namespace, path);
+        return registry.get(id);
+    }
+
+    /**
+     * Utility method to list suggestions for a given registry.
+     * @param registry the registry to list suggestions for
+     * @param builder the SuggestionsBuilder to submit suggestions to
+     * @return a CompletableFuture that will complete with the suggestions
+     */
+    public static CompletableFuture<Suggestions> listSuggestions(Registrar<?> registry, SuggestionsBuilder builder) {
+        final String input = builder.getRemainingLowerCase();
+
+        registry.getIds().stream()
+                .filter(id -> JCraft.MOD_ID.equals(id.getNamespace()) && id.getPath().startsWith(input) ||
+                        id.toString().startsWith(input))
+                .map(ResourceLocation::toString)
+                .forEach(builder::suggest);
+
+        return builder.buildFuture();
     }
 }
--- common/src/main/java/net/arna/jcraft/common/argumenttype/StandArgumentType.java
+++ common/src/main/java/net/arna/jcraft/common/argumenttype/StandArgumentType.java
@@ -1,7 +1,6 @@
 package net.arna.jcraft.common.argumenttype;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.context.CommandContext;
@@ -14,35 +13,28 @@ import lombok.NoArgsConstructor;
 import net.arna.jcraft.api.JRegistries;
 import net.arna.jcraft.api.stand.StandType;
 import net.minecraft.network.chat.Component;
+
 import java.util.Collection;
-import java.util.Locale;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 @NoArgsConstructor(staticName = "stand")
 public class StandArgumentType implements ArgumentType<StandType> {
     public static final SimpleCommandExceptionType NOT_FOUND = new SimpleCommandExceptionType(Component.literal("That stand was not found"));
-    private static final Map<String, StandType> suggestions = JRegistries.STAND_TYPE_REGISTRY.entrySet().stream()
-            .collect(ImmutableMap.toImmutableMap(entry -> entry.getKey().location().getPath().toLowerCase(Locale.ROOT).replaceAll("_", ""), Map.Entry::getValue));
     @Getter // implements ArgumentType#getExamples()
     private final Collection<String> examples = ImmutableList.of("MADE_IN_HEAVEN", "C_MOON", "GER");
 
     @Override
     public StandType parse(final StringReader reader) throws CommandSyntaxException {
-        final String name = reader.readUnquotedString();
-        final StandType type = suggestions.get(name);
+        StandType type = JRegistries.parseRegistryEntry(JRegistries.STAND_TYPE_REGISTRY, reader);
         if (type == null) {
             throw NOT_FOUND.createWithContext(reader);
         }
+
         return type;
     }
 
     @Override
     public <S> CompletableFuture<Suggestions> listSuggestions(final CommandContext<S> context, final SuggestionsBuilder builder) {
-        final String input = builder.getRemainingLowerCase().replaceAll("_", "");
-        suggestions.keySet().stream()
-                .filter(standType -> standType.startsWith(input))
-                .forEach(builder::suggest);
-        return builder.buildFuture();
+        return JRegistries.listSuggestions(JRegistries.STAND_TYPE_REGISTRY, builder);
     }
 }
