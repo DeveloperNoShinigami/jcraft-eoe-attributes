ed0bc4f7f77f550d783a1c4fcd50e384b9154415
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2025-06-09 21:44:45 +0200

    Create datagen api classes

---
+++ fabric/src/main/java/net/arna/jcraft/fabric/api/MoveSetProvider.java
@@ -0,0 +1,53 @@
+package net.arna.jcraft.fabric.api;
+
+import com.mojang.serialization.Codec;
+import lombok.Getter;
+import net.arna.jcraft.api.IAttackerType;
+import net.arna.jcraft.api.attack.MoveSet;
+import net.arna.jcraft.common.attack.core.IAttacker;
+import net.arna.jcraft.common.attack.core.MoveClass;
+import net.arna.jcraft.common.attack.core.MoveMap;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricCodecDataProvider;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+@Getter
+public abstract class MoveSetProvider<A extends IAttacker<A, S>, S extends Enum<S>> extends FabricCodecDataProvider<MoveMap.Entry<A, S>> {
+    private final ResourceLocation type;
+
+    protected MoveSetProvider(FabricDataOutput dataOutput, Codec<MoveMap.Entry<A, S>> codec, ResourceLocation type) {
+        super(dataOutput, PackOutput.Target.DATA_PACK, "movesets", codec);
+        this.type = type;
+    }
+
+    @Override
+    protected final void configure(BiConsumer<ResourceLocation, MoveMap.Entry<A, S>> provider) {
+        configureMoveSets(set -> {
+            IAttackerType type = set.getType();
+            ResourceLocation base = type.getId().withPath(p -> String.format("%s/%s", type.kind(), p));
+            MoveMap<A, S> map = set.save();
+
+            for (Map.Entry<MoveClass, MoveMap.Entry<A, S>> entry : map.getEntries().entries()) {
+                String moveClass = entry.getKey().getName();
+                String moveName = entry.getValue().getMove().getName().getString()
+                        .toLowerCase().replace(" ", "_").replaceAll("[^a-z0-9/._-]", "");
+                ResourceLocation id = base.withPath(p -> "%s/%s/%s".formatted(p, moveClass, moveName));
+
+                provider.accept(id, entry.getValue());
+            }
+        });
+    }
+
+    protected abstract void configureMoveSets(Consumer<MoveSet<A, S>> provider);
+
+    @Override
+    public @NotNull String getName() {
+        return "Move Sets for " + type;
+    }
+}
--- fabric/src/main/java/net/arna/jcraft/fabric/datagen/JPoseProvider.java
+++ fabric/src/main/java/net/arna/jcraft/fabric/datagen/JPoseProvider.java
@@ -2,48 +2,26 @@ package net.arna.jcraft.fabric.datagen;
 
 import lombok.Getter;
 import net.arna.jcraft.api.pose.ModelType;
-import net.arna.jcraft.api.pose.PoseModifiers;
-import net.arna.jcraft.api.pose.modifier.IPoseModifier;
 import net.arna.jcraft.common.entity.stand.*;
+import net.arna.jcraft.fabric.api.PoseProvider;
 import net.arna.jcraft.registry.JStandTypeRegistry;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricCodecDataProvider;
-import net.minecraft.data.PackOutput;
-import net.minecraft.resources.ResourceLocation;
-
-import java.util.function.BiConsumer;
 
 @Getter
-public class JPoseProvider extends FabricCodecDataProvider<IPoseModifier> {
+public class JPoseProvider extends PoseProvider {
     private final String name = "Poses";
 
     public JPoseProvider(FabricDataOutput dataOutput) {
-        super(dataOutput, PackOutput.Target.RESOURCE_PACK, "poses", PoseModifiers.CODEC);
+        super(dataOutput);
     }
 
     @Override
-    protected void configure(BiConsumer<ResourceLocation, IPoseModifier> provider) {
-        registerPoses((id, modelType, pose) -> {
-            if (!pose.isModelSupported(modelType)) {
-                throw new IllegalArgumentException("Pose " + id + " is not supported by model type " + modelType);
-            }
-
-            ResourceLocation absId = id.withPath(p -> p + "/" + modelType.getName());
-            provider.accept(absId, pose);
-        });
-    }
-
-    private void registerPoses(PoseRegistrar registrar) {
+    protected void registerPoses(PoseRegistrar registrar) {
         registrar.register(JStandTypeRegistry.GOLD_EXPERIENCE_REQUIEM.getId(), ModelType.HUMANOID, GEREntity.POSE);
         registrar.register(JStandTypeRegistry.KING_CRIMSON.getId(), ModelType.HUMANOID, KingCrimsonEntity.POSE);
         registrar.register(JStandTypeRegistry.KILLER_QUEEN_BITES_THE_DUST.getId(), ModelType.HUMANOID, KQBTDEntity.POSE);
         registrar.register(JStandTypeRegistry.KILLER_QUEEN.getId(), ModelType.HUMANOID, KillerQueenEntity.POSE);
         registrar.register(JStandTypeRegistry.STAR_PLATINUM.getId(), ModelType.HUMANOID, StarPlatinumEntity.POSE);
         registrar.register(JStandTypeRegistry.THE_WORLD_OVER_HEAVEN.getId(), ModelType.HUMANOID, TheWorldOverHeavenEntity.POSE);
-    }
-
-    @FunctionalInterface
-    private interface PoseRegistrar {
-        void register(ResourceLocation id, ModelType<?> modelType, IPoseModifier pose);
     }
 }
