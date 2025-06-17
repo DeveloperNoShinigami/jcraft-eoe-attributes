b70319cde5936f1b3d82b99d9d33e59ce72b878a
ayutac <fly.high.android@gmail.com>
2025-06-18 01:00:31 +0200

    Revert "Tried to give Metallica and Mandom no hitbox."
    This reverts commit 1bced328ba5f140a0edb96948b812d88fa0d8f05.

--- common/src/main/java/net/arna/jcraft/common/entity/stand/MetallicaEntity.java
+++ common/src/main/java/net/arna/jcraft/common/entity/stand/MetallicaEntity.java
@@ -49,9 +49,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3f;
 
@@ -462,12 +460,6 @@ public class MetallicaEntity extends StandEntity<MetallicaEntity, MetallicaEntit
                     toUser.x, toUser.y, toUser.z
             );
         }
-    }
-
-    @NotNull
-    @Override
-    protected AABB makeBoundingBox() {
-        return AABB.ofSize(getPosition(0f), 0d, 0d, 0d);
     }
 
     @Override
