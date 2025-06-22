0d5a92f9848a9ad0c08ca1bcf728fb670e8d51de
ayutac <fly.high.android@gmail.com>
2025-06-22 22:44:18 +0200

    Added Entity and Living Entity to Extractors.

--- common/src/main/java/net/arna/jcraft/common/marker/Extractors.java
+++ common/src/main/java/net/arna/jcraft/common/marker/Extractors.java
@@ -1,9 +1,21 @@
 package net.arna.jcraft.common.marker;
 
+import com.mojang.serialization.DataResult;
+import net.arna.jcraft.common.util.NbtUtils;
 import net.arna.jcraft.common.util.TriConsumer;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.StringTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.phys.Vec3;
+
+import static net.arna.jcraft.common.marker.Identifiers.*;
 
 public interface Extractors {
 
@@ -11,10 +23,126 @@ public interface Extractors {
         if (id == null) {
             return;
         }
-        // TODO continue
-        return;
+        if (id.equals(POSITION)) {
+            final Entity vehicle = entity.getVehicle();
+            if (vehicle != null) {
+                NbtUtils.put(compoundTag, POSITION.toString(), new Vec3(vehicle.getX(), vehicle.getY(), vehicle.getZ()));
+            }
+            else {
+                NbtUtils.put(compoundTag, POSITION.toString(), new Vec3(entity.getX(), entity.getY(), entity.getZ()));
+            }
+        }
+        else if (id.equals(VELOCITY)) {
+            NbtUtils.put(compoundTag, VELOCITY.toString(), entity.getDeltaMovement());
+        }
+        else if (id.equals(PITCH)) {
+            compoundTag.putFloat(PITCH.toString(), entity.getXRot());
+        }
+        else if (id.equals(YAW)) {
+            compoundTag.putFloat(YAW.toString(), entity.getYRot());
+        }
+        else if (id.equals(YAW_HEAD)) {
+            compoundTag.putFloat(YAW_HEAD.toString(), entity.getYHeadRot());
+        }
+        else if (id.equals(FALL_DISTANCE)) {
+            compoundTag.putFloat(FALL_DISTANCE.toString(), entity.fallDistance);
+        }
+        else if (id.equals(FIRE)) {
+            compoundTag.putInt(FIRE.toString(), entity.getRemainingFireTicks());
+        }
+        else if (id.equals(AIR)) {
+            compoundTag.putInt(AIR.toString(), entity.getAirSupply());
+        }
+        else if (id.equals(GROUNDED)) {
+            compoundTag.putBoolean(GROUNDED.toString(), entity.onGround());
+        }
+        else if (id.equals(INVULNERABLE)) {
+            compoundTag.putBoolean(INVULNERABLE.toString(), entity.isInvulnerable());
+        }
+        else if (id.equals(PORTAL_COOLDOWN)) {
+            compoundTag.putInt(PORTAL_COOLDOWN.toString(), entity.getPortalCooldown());
+        }
+        else if (id.equals(UUID)) {
+            compoundTag.putUUID(UUID.toString(), entity.getUUID());
+        }
+        else if (id.equals(CUSTOM_NAME)) {
+            final Component component = entity.getCustomName();
+            if (component != null) {
+                compoundTag.putString(CUSTOM_NAME.toString(), Component.Serializer.toJson(component));
+            }
+        }
+        else if (id.equals(CUSTOM_NAME_VISIBLE)) {
+            compoundTag.putBoolean(CUSTOM_NAME_VISIBLE.toString(), entity.isCustomNameVisible());
+        }
+        else if (id.equals(SILENT)) {
+            compoundTag.putBoolean(SILENT.toString(), entity.isSilent());
+        }
+        else if (id.equals(NO_GRAVITY)) {
+            compoundTag.putBoolean(NO_GRAVITY.toString(), entity.isNoGravity());
+        }
+        else if (id.equals(GLOWING)) {
+            compoundTag.putBoolean(GLOWING.toString(), entity.hasGlowingTag());
+        }
+        else if (id.equals(TICKS_FROZEN)) {
+            compoundTag.putInt(TICKS_FROZEN.toString(), entity.getTicksFrozen());
+        }
+        else if (id.equals(VISUAL_FIRE)) {
+            compoundTag.putBoolean(VISUAL_FIRE.toString(), entity.hasGlowingTag());
+        }
+        else if (id.equals(TAGS)) {
+            final ListTag listTag = new ListTag();
+            for (final String string : entity.getTags()) {
+                listTag.add(StringTag.valueOf(string));
+            }
+            compoundTag.put(TAGS.toString(), listTag);
+        }
+        // TODO passengers?
     };
 
-    // TODO LIVING_ENTITY etc
+    TriConsumer<ResourceLocation, Entity, CompoundTag> LIVING_ENTITY = (id, entity, compoundTag) -> {
+        ENTITY.accept(id, entity, compoundTag);
+        if (id == null || !(entity instanceof final LivingEntity livingEntity)) {
+            return;
+        }
+        if (id.equals(HEALTH)) {
+            compoundTag.putFloat(HEALTH.toString(), livingEntity.getHealth());
+        }
+        else if (id.equals(HURT_TIME)) {
+            compoundTag.putInt(HURT_TIME.toString(), livingEntity.hurtTime);
+        }
+        else if (id.equals(HURT_BY_TIMESTAMP)) {
+            compoundTag.putInt(HURT_BY_TIMESTAMP.toString(), livingEntity.getLastHurtByMobTimestamp());
+        }
+        else if (id.equals(DEATH_TIME)) {
+            compoundTag.putInt(DEATH_TIME.toString(), livingEntity.deathTime);
+        }
+        else if (id.equals(ABSORPTION_AMOUNT)) {
+            compoundTag.putFloat(ABSORPTION_AMOUNT.toString(), livingEntity.getAbsorptionAmount());
+        }
+        else if (id.equals(ATTRIBUTES)) {
+            compoundTag.put(ATTRIBUTES.toString(), livingEntity.getAttributes().save());
+        }
+        else if (id.equals(ACTIVE_EFFECTS)) {
+            final ListTag listTag = new ListTag();
+            for (final MobEffectInstance mobEffectInstance : livingEntity.getActiveEffects()) {
+                listTag.add(mobEffectInstance.save(new CompoundTag()));
+            }
+            compoundTag.put(ACTIVE_EFFECTS.toString(), listTag);
+        }
+        else if (id.equals(FALL_FLYING)) {
+            compoundTag.putBoolean(FALL_FLYING.toString(), livingEntity.isFallFlying());
+        }
+        else if (id.equals(SLEEPING_POSITION) && livingEntity.getSleepingPos().isPresent()) {
+            NbtUtils.put(compoundTag, SLEEPING_POSITION.toString(), livingEntity.getSleepingPos().get());
+        }
+        else if (id.equals(BRAIN)) {
+            final DataResult<Tag> dataResult = livingEntity.getBrain().serializeStart(NbtOps.INSTANCE);
+            dataResult.result().ifPresent(tag -> compoundTag.put(BRAIN.toString(), tag));
+        }
+    };
+
+    TriConsumer<ResourceLocation, Entity, CompoundTag> MOB = (id, entity, compoundTag) -> {
+
+    };
 
 }
--- common/src/main/java/net/arna/jcraft/common/marker/Identifiers.java
+++ common/src/main/java/net/arna/jcraft/common/marker/Identifiers.java
@@ -1,4 +1,42 @@
 package net.arna.jcraft.common.marker;
 
+import net.minecraft.resources.ResourceLocation;
+
 public interface Identifiers {
+
+    String MINECRAFT = "minecraft";
+
+    ResourceLocation POSITION = new ResourceLocation(MINECRAFT, "pos");
+    ResourceLocation VELOCITY = new ResourceLocation(MINECRAFT, "vel");
+    ResourceLocation PITCH = new ResourceLocation(MINECRAFT, "xrot");
+    ResourceLocation YAW = new ResourceLocation(MINECRAFT, "yrot");
+    ResourceLocation YAW_HEAD = new ResourceLocation(MINECRAFT, "yheadrot");
+    ResourceLocation FALL_DISTANCE = new ResourceLocation(MINECRAFT, "fall_distance");
+    ResourceLocation FIRE = new ResourceLocation(MINECRAFT, "fire");
+    ResourceLocation AIR = new ResourceLocation(MINECRAFT, "air");
+    ResourceLocation GROUNDED = new ResourceLocation(MINECRAFT, "grounded");
+    ResourceLocation INVULNERABLE = new ResourceLocation(MINECRAFT, "invulnerable");
+    ResourceLocation PORTAL_COOLDOWN = new ResourceLocation(MINECRAFT, "portal_cooldown");
+    ResourceLocation UUID = new ResourceLocation(MINECRAFT, "uuid");
+    ResourceLocation CUSTOM_NAME = new ResourceLocation(MINECRAFT, "custom_name");
+    ResourceLocation CUSTOM_NAME_VISIBLE = new ResourceLocation(MINECRAFT, "custom_name_visible");
+    ResourceLocation SILENT = new ResourceLocation(MINECRAFT, "silent");
+    ResourceLocation NO_GRAVITY = new ResourceLocation(MINECRAFT, "no_gravity");
+    ResourceLocation GLOWING = new ResourceLocation(MINECRAFT, "glowing");
+    ResourceLocation TICKS_FROZEN = new ResourceLocation(MINECRAFT, "ticks_frozen");
+    ResourceLocation VISUAL_FIRE = new ResourceLocation(MINECRAFT, "visual_fire");
+    ResourceLocation TAGS = new ResourceLocation(MINECRAFT, "tags");
+    ResourceLocation HEALTH = new ResourceLocation(MINECRAFT, "health");
+    ResourceLocation HURT_TIME = new ResourceLocation(MINECRAFT, "hurt_time");
+    ResourceLocation HURT_BY_TIMESTAMP = new ResourceLocation(MINECRAFT, "hurt_by_timestamp");
+    ResourceLocation DEATH_TIME = new ResourceLocation(MINECRAFT, "death_time");
+    ResourceLocation ABSORPTION_AMOUNT = new ResourceLocation(MINECRAFT, "absorption_amount");
+    ResourceLocation ATTRIBUTES = new ResourceLocation(MINECRAFT, "attributes");
+    ResourceLocation ACTIVE_EFFECTS = new ResourceLocation(MINECRAFT, "active_effects");
+    ResourceLocation FALL_FLYING = new ResourceLocation(MINECRAFT, "active_effects");
+    ResourceLocation SLEEPING_POSITION = new ResourceLocation(MINECRAFT, "sleeping_pos");
+    ResourceLocation BRAIN = new ResourceLocation(MINECRAFT, "brain");
+
+
+
 }
