a9f0fc64ab48280a1381ef7d46a0b5a8ba49dccf
ayutac <fly.high.android@gmail.com>
2025-06-25 00:10:06 +0200

    Cleaned Imports.

--- common/src/main/java/net/arna/jcraft/common/attack/moves/mandom/RewindMove.java
+++ common/src/main/java/net/arna/jcraft/common/attack/moves/mandom/RewindMove.java
@@ -27,7 +27,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.UUID;
 
 public final class RewindMove extends AbstractMove<RewindMove, MandomEntity> {
 
