560cc2e4e26a1b330e4440888179d4cec8ce8504
XircTwerk <ddanielcohen2010@gmail.com>
2025-07-13 13:56:56 -0700

    used jcraft.id

--- common/src/main/java/net/arna/jcraft/client/model/entity/TrainingDummyModel.java
+++ common/src/main/java/net/arna/jcraft/client/model/entity/TrainingDummyModel.java
@@ -1,6 +1,7 @@
 package net.arna.jcraft.client.model.entity;
 
 import mod.azure.azurelib.model.GeoModel;
+import net.arna.jcraft.JCraft;
 import net.arna.jcraft.common.entity.TrainingDummyEntity;
 import net.minecraft.resources.ResourceLocation;
 
@@ -11,16 +12,16 @@ public class TrainingDummyModel extends GeoModel<TrainingDummyEntity> {
 
     @Override
     public ResourceLocation getModelResource(TrainingDummyEntity animatable) {
-        return new ResourceLocation("jcraft", "geo/training_dummy.geo.json");
+        return JCraft.id("geo/training_dummy.geo.json");
     }
 
     @Override
     public ResourceLocation getTextureResource(TrainingDummyEntity animatable) {
-        return new ResourceLocation("jcraft", "textures/entity/training_dummy.png");
+        return JCraft.id("textures/entity/training_dummy.png");
     }
 
     @Override
     public ResourceLocation getAnimationResource(TrainingDummyEntity animatable) {
-        return new ResourceLocation("jcraft", "animations/training_dummy.animation.json");
+        return JCraft.id("animations/training_dummy.animation.json");
     }
 }
