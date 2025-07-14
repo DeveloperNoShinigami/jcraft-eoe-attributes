d18e591b9166874a547d086528beb29d908a8512
Arna577 <108146160+Arna577@users.noreply.github.com>
2025-07-14 14:46:07 +0200

    made dummy not a valid AI stand user

--- datagen/src/main/java/net/arna/jcraft/datagen/providers/data/JTagProviders.java
+++ datagen/src/main/java/net/arna/jcraft/datagen/providers/data/JTagProviders.java
@@ -684,19 +684,22 @@ public class JTagProviders {
                 stands.add(BuiltInRegistries.ENTITY_TYPE.getKey(stand.getEntityType()));
             }
 
-            final var never_stands = getOrCreateTagBuilder(JTagRegistry.CAN_NEVER_HAVE_STAND);
-            never_stands.addTag(JTagRegistry.STANDS);
-            never_stands.add(JEntityTypeRegistry.SHEER_HEART_ATTACK.getId());
-            never_stands.add(JEntityTypeRegistry.LIFE_DETECTOR.getId());
-            never_stands.add(JEntityTypeRegistry.GE_FROG.getId());
-            never_stands.add(JEntityTypeRegistry.GE_BUTTERFLY.getId());
-            never_stands.add(JEntityTypeRegistry.GE_SNAKE.getId());
-            never_stands.add(JEntityTypeRegistry.GER_SCORPION.getId());
-            never_stands.add(JEntityTypeRegistry.PLAYER_CLONE.getId());
-            never_stands.add(JEntityTypeRegistry.HG_NET.getId());
-            never_stands.add(JEntityTypeRegistry.RED_BIND.getId());
-            never_stands.add(JEntityTypeRegistry.SAND_TORNADO.getId());
-            never_stands.add(JEntityTypeRegistry.STAND_METEOR.getId());
+            final var neverStands = getOrCreateTagBuilder(JTagRegistry.CAN_NEVER_HAVE_STAND);
+            neverStands.addTag(JTagRegistry.STANDS);
+            neverStands.add(JEntityTypeRegistry.SHEER_HEART_ATTACK.getId());
+            neverStands.add(JEntityTypeRegistry.LIFE_DETECTOR.getId());
+            neverStands.add(JEntityTypeRegistry.GE_FROG.getId());
+            neverStands.add(JEntityTypeRegistry.GE_BUTTERFLY.getId());
+            neverStands.add(JEntityTypeRegistry.GE_SNAKE.getId());
+            neverStands.add(JEntityTypeRegistry.GER_SCORPION.getId());
+            neverStands.add(JEntityTypeRegistry.PLAYER_CLONE.getId());
+            neverStands.add(JEntityTypeRegistry.HG_NET.getId());
+            neverStands.add(JEntityTypeRegistry.RED_BIND.getId());
+            neverStands.add(JEntityTypeRegistry.SAND_TORNADO.getId());
+            neverStands.add(JEntityTypeRegistry.STAND_METEOR.getId());
+
+            final var noAIStandUsers = getOrCreateTagBuilder(JTagRegistry.NO_STAND_USER_AI);
+            noAIStandUsers.add(JEntityTypeRegistry.TRAINING_DUMMY.getId());
 
             addTagsForCompatibilities(arg);
         }
