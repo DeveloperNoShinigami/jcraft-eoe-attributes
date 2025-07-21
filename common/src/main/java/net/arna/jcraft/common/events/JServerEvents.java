a2fd55867ec12988f6afddd154571bea6bec3c07
XircTwerk <ddanielcohen2010@gmail.com>
2025-07-21 13:15:55 -0700

    resolved merge conflicts

--- common/src/main/java/net/arna/jcraft/common/events/JServerEvents.java
+++ common/src/main/java/net/arna/jcraft/common/events/JServerEvents.java
@@ -2,8 +2,6 @@ package net.arna.jcraft.common.events;
 
 import dev.architectury.event.CompoundEventResult;
 import dev.architectury.event.EventResult;
-import dev.architectury.event.events.common.InteractionEvent;
-import dev.architectury.event.events.common.PlayerEvent;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import net.arna.jcraft.JCraft;
@@ -21,7 +19,6 @@ import net.arna.jcraft.common.entity.StandMeteorEntity;
 import net.arna.jcraft.api.stand.StandEntity;
 import net.arna.jcraft.common.gravity.api.GravityChangerAPI;
 import net.arna.jcraft.common.item.MockItem;
-import net.arna.jcraft.common.item.Peacemaker;
 import net.arna.jcraft.common.network.s2c.AttackerDataPacket;
 import net.arna.jcraft.common.saveddata.ExclusiveStandsData;
 import net.arna.jcraft.common.tickable.*;
@@ -97,6 +94,7 @@ public class JServerEvents {
             JCraft.preloadLockTicks--;
         }
 
+        //noinspection removal
         RevolverFire.tick(server);
         PeacemakerReload.tick(server);
         PastDimensions.tick(server);
