e4be7c96e9e5ce7e8dff1b1239fdfa352e2b6b59
ayutac <fly.high.android@gmail.com>
2025-08-26 20:00:19 +0200

    Better solution for that bug.

--- common/src/main/java/net/arna/jcraft/common/entity/TrainingDummyEntity.java
+++ common/src/main/java/net/arna/jcraft/common/entity/TrainingDummyEntity.java
@@ -63,11 +63,7 @@ public class TrainingDummyEntity extends Mob implements GeoEntity, ICustomDamage
     public TrainingDummyEntity(EntityType<? extends TrainingDummyEntity> entityType, Level level) {
         super(entityType, level);
         this.setMaxUpStep(0.0F);
-    }
-
-    @Override
-    public boolean isPersistenceRequired() {
-        return true;
+        setPersistenceRequired();
     }
 
     public static AttributeSupplier.@NotNull Builder createLivingAttributes() {
