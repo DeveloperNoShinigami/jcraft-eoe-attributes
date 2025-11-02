b57fa4143531ffeb035a12e166759e8f2c313b09
Ayutac <fly.high.android@gmail.com>
2025-11-02 10:47:11 +0100

    Made MRGlowLayer work again.

--- common/src/main/java/net/arna/jcraft/client/renderer/entity/layer/MRGlowLayer.java
+++ common/src/main/java/net/arna/jcraft/client/renderer/entity/layer/MRGlowLayer.java
@@ -1,17 +1,12 @@
 package net.arna.jcraft.client.renderer.entity.layer;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import mod.azure.azurelib.model.AzBone;
+import lombok.NonNull;
 import mod.azure.azurelib.render.AzRendererPipelineContext;
-import mod.azure.azurelib.render.layer.AzRenderLayer;
 import net.arna.jcraft.JCraft;
 import net.arna.jcraft.common.entity.stand.MagiciansRedEntity;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 
 import java.util.UUID;
@@ -20,12 +15,13 @@ import java.util.UUID;
 public class MRGlowLayer extends AbstractRenderLayer<MagiciansRedEntity> {
     private static final ResourceLocation LAYER = new ResourceLocation(JCraft.MOD_ID, "textures/entity/stands/magicians_red/glow.png");
 
-    /*@Override
-    public void render(final PoseStack poseStack, final MagiciansRedEntity animatable, final BakedGeoModel bakedModel, final RenderType renderType,
-                       final MultiBufferSource bufferSource, final VertexConsumer bufferIn, final float partialTick, final int packedLight, final int packedOverlay) {
+    @Override
+    public void render(final @NonNull AzRendererPipelineContext<UUID, MagiciansRedEntity> pc) {
         final RenderType cameo = RenderType.eyes(LAYER);
+        pc.setRenderType(cameo);
+        pc.setVertexConsumer(pc.multiBufferSource().getBuffer(cameo));
+        setDefaultGlow(pc);
+        pc.rendererPipeline().reRender(pc);
+    }
 
-        getRenderer().reRender(bakedModel, poseStack, bufferSource, animatable, cameo,
-                bufferSource.getBuffer(cameo), partialTick, 15728640, OverlayTexture.NO_OVERLAY, 1, 1, 1, 1f);
-    }*/
 }
