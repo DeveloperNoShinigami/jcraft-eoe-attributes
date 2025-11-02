574add82b53828604b3db0b58d086f07ab448556
Ayutac <fly.high.android@gmail.com>
2025-11-02 10:16:55 +0100

    Made CreamVoidLayer work again I think.

--- common/src/main/java/net/arna/jcraft/client/renderer/entity/layer/CreamVoidLayer.java
+++ common/src/main/java/net/arna/jcraft/client/renderer/entity/layer/CreamVoidLayer.java
@@ -1,15 +1,11 @@
 package net.arna.jcraft.client.renderer.entity.layer;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import mod.azure.azurelib.model.AzBone;
+import lombok.NonNull;
 import mod.azure.azurelib.render.AzRendererPipelineContext;
-import mod.azure.azurelib.render.layer.AzRenderLayer;
 import net.arna.jcraft.JCraft;
 import net.arna.jcraft.common.entity.stand.CreamEntity;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
@@ -20,20 +16,25 @@ import java.util.stream.IntStream;
 
 @Environment(EnvType.CLIENT)
 public class CreamVoidLayer extends AbstractRenderLayer<CreamEntity> {
-    private static final List<ResourceLocation> skins = IntStream.range(0, 4).mapToObj(
+    private static final List<ResourceLocation> SKINS = IntStream.range(0, 4).mapToObj(
             i -> JCraft.id("textures/entity/stands/cream/void" + i + ".png")).toList();
 
-    /*public CreamVoidLayer(final GeoRenderer<CreamEntity> entityRendererIn) {
-        super(entityRendererIn);
+    @Override
+    public void render(final @NonNull AzRendererPipelineContext<UUID, CreamEntity> pc) {
+        final CreamEntity cream = pc.animatable();
+        if (cream.tickCount == 0) {
+            return;
+        }
+        final RenderType cameo = RenderType.dragonExplosionAlpha(SKINS.get(cream.getSkin()));
+        pc.setRenderType(cameo);
+        pc.setVertexConsumer(pc.multiBufferSource().getBuffer(cameo));
+        pc.setPackedLight(15728640);
+        pc.setPackedLight(OverlayTexture.NO_OVERLAY);
+        pc.setRed(1f);
+        pc.setGreen(1f);
+        pc.setBlue(1f);
+        pc.setAlpha(1f);
+        pc.rendererPipeline().reRender(pc);
     }
 
-    @Override
-    public void render(final PoseStack poseStack, final CreamEntity animatable, final BakedGeoModel bakedModel, final RenderType renderType,
-                       final MultiBufferSource bufferSource, final VertexConsumer buffer, final float partialTick, final int packedLight, final int packedOverlay) {
-        if (animatable.tickCount == 0) return; // Prevents summoning anim flashing players screen
-        final RenderType cameo = RenderType.dragonExplosionAlpha(skins.get(animatable.getSkin()));
-
-        getRenderer().reRender(bakedModel, poseStack, bufferSource, animatable, cameo,
-                bufferSource.getBuffer(cameo), partialTick, 15728640, OverlayTexture.NO_OVERLAY, 1, 1, 1, 1f);
-    }*/
 }
