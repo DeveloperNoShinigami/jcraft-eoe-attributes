3b3928d0d163bc9adb2266a146975b8d5effc1ce
Ayutac <fly.high.android@gmail.com>
2025-11-02 21:33:46 +0100

    Fixed large icicle texture and tried/failed to rotate the projectiles.

--- common/src/main/java/net/arna/jcraft/client/renderer/entity/projectiles/LargeIcicleRenderer.java
+++ common/src/main/java/net/arna/jcraft/client/renderer/entity/projectiles/LargeIcicleRenderer.java
@@ -1,54 +1,46 @@
 package net.arna.jcraft.client.renderer.entity.projectiles;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 import lombok.NonNull;
-import net.arna.jcraft.client.model.JProjectileModel;
+import mod.azure.azurelib.render.AzRendererPipelineContext;
+import net.arna.jcraft.JCraft;
 import net.arna.jcraft.client.renderer.entity.AbstractEntityRenderer;
 import net.arna.jcraft.common.entity.projectile.LargeIcicleProjectile;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
-import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
+import java.util.function.Function;
 
 /**
  * The {@link AbstractEntityRenderer} for {@link LargeIcicleProjectile}.
  */
 @Environment(EnvType.CLIENT)
-public class LargeIcicleRenderer extends AbstractEntityRenderer<LargeIcicleProjectile> {
+public class LargeIcicleRenderer extends ProjectileRenderer<LargeIcicleProjectile> {
 
     public static final String ID = "large_icicle";
 
-    public LargeIcicleRenderer(final @NonNull EntityRendererProvider.Context context) {
-        super(context, () -> new EntityAnimator<>(ID), ID);
+    public LargeIcicleRenderer(final @NonNull EntityRendererProvider.Context pc) {
+        super(pc, () -> new EntityAnimator<>(ID), b -> b
+                .setRenderType(RenderType.entityTranslucent(JCraft.id(TEXTURE_STR_TEMPLATE.formatted(ID))))
+                .setPrerenderEntry(preRenderEntry()),
+                ID);
     }
 
-    @Override
-    public boolean shouldShowName(final @NonNull LargeIcicleProjectile animatable) {
-        return false;
+    protected static Function<AzRendererPipelineContext<UUID, LargeIcicleProjectile>, AzRendererPipelineContext<UUID, LargeIcicleProjectile>> preRenderEntry() {
+        return pc -> {
+            final LargeIcicleProjectile animatable = pc.animatable();
+            final PoseStack poseStack = pc.poseStack();
+            poseStack.mulPose(Axis.YP.rotationDegrees(Mth.lerp(pc.partialTick(), animatable.yRotO, animatable.getYRot()) + 90));
+            poseStack.mulPose(Axis.ZN.rotationDegrees(Mth.lerp(pc.partialTick(), animatable.xRotO, animatable.getXRot())));
+            final float scale = animatable.getScale();
+            poseStack.scale(scale, scale, scale);
+            return pc;
+        };
     }
 
-    /*
-    @Override
-    public RenderType getRenderType(final @NonNull LargeIcicleProjectile animatable, final ResourceLocation texture,
-                                    final @Nullable MultiBufferSource bufferSource, final float partialTick) {
-        return RenderType.entityTranslucent(texture);
-    }
-
-    @Override
-    public void preRender(final PoseStack poseStack, final LargeIcicleProjectile animatable, final BakedGeoModel model, final MultiBufferSource bufferSource,
-                          final VertexConsumer buffer, final boolean isReRender, final float partialTick, final int packedLight, final int packedOverlay,
-                          final float red, final float green, final float blue, final float alpha) {
-        // No-clipped/.noPhysics() projectiles get inverted pitch, for some reason.
-        poseStack.mulPose(Axis.YP.rotationDegrees(Mth.lerp(partialTick, animatable.yRotO, animatable.getYRot()) + 90));
-        poseStack.mulPose(Axis.ZP.rotationDegrees(Mth.lerp(partialTick, animatable.xRotO, animatable.getXRot())));
-        float scale = animatable.getScale();
-        poseStack.scale(scale, scale, scale);
-        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
-    }*/
 }
--- common/src/main/java/net/arna/jcraft/client/renderer/entity/projectiles/ProjectileRenderer.java
+++ common/src/main/java/net/arna/jcraft/client/renderer/entity/projectiles/ProjectileRenderer.java
@@ -1,7 +1,10 @@
 package net.arna.jcraft.client.renderer.entity.projectiles;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
 import lombok.NonNull;
 import mod.azure.azurelib.animation.AzAnimator;
+import mod.azure.azurelib.render.AzRendererPipelineContext;
 import mod.azure.azurelib.render.entity.AzEntityRendererConfig;
 import net.arna.jcraft.JCraft;
 import net.arna.jcraft.client.renderer.entity.AbstractEntityRenderer;
@@ -9,9 +12,11 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 
 import java.util.UUID;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
@@ -19,28 +24,44 @@ import java.util.function.UnaryOperator;
 public class ProjectileRenderer<T extends Entity> extends AbstractEntityRenderer<T> {
     protected static final String TEXTURE_STR_TEMPLATE = "textures/entity/projectiles/%s.png";
 
-    public ProjectileRenderer(final @NonNull EntityRendererProvider.Context context, final @NonNull String id) {
-        this(context, () -> new EntityAnimator<>(id), id);
+    public ProjectileRenderer(final EntityRendererProvider.Context context, final @NonNull Supplier<AzAnimator<UUID,T>> azAnimatorSupplier, final @NonNull UnaryOperator<AzEntityRendererConfig.Builder<T>> additionalConfigs, final ResourceLocation model, final ResourceLocation texture) {
+        super(context, azAnimatorSupplier, additionalConfigs.compose(b -> b
+                .setPrerenderEntry(preRenderEntry())),
+                model, texture);
     }
 
     public ProjectileRenderer(final @NonNull EntityRendererProvider.Context context, final @NonNull Supplier<AzAnimator<UUID, T>> azAnimatorSupplier, final @NonNull UnaryOperator<AzEntityRendererConfig.Builder<T>> additionalConfigs, final @NonNull String id) {
-        super(context, azAnimatorSupplier, additionalConfigs, JCraft.id(MODEL_STR_TEMPLATE.formatted(id)), JCraft.id(TEXTURE_STR_TEMPLATE.formatted(id)));
-    }
-
-    public ProjectileRenderer(final @NonNull AzEntityRendererConfig<T> config, final @NonNull EntityRendererProvider.Context context, final @NonNull ResourceLocation model, final @NonNull ResourceLocation texture) {
-        super(config, context, model, texture);
-    }
-
-    public ProjectileRenderer(final @NonNull AzEntityRendererConfig<T> config, final @NonNull EntityRendererProvider.Context context, final @NonNull String id) {
-        this(config, context, JCraft.id(MODEL_STR_TEMPLATE.formatted(id)), JCraft.id(TEXTURE_STR_TEMPLATE.formatted(id)));
+        this(context, azAnimatorSupplier, additionalConfigs, JCraft.id(MODEL_STR_TEMPLATE.formatted(id)), JCraft.id(TEXTURE_STR_TEMPLATE.formatted(id)));
     }
 
     public ProjectileRenderer(final @NonNull EntityRendererProvider.Context context, final @NonNull Supplier<AzAnimator<UUID, T>> azAnimatorSupplier, final @NonNull ResourceLocation model, final @NonNull ResourceLocation texture) {
-        super(context, azAnimatorSupplier, model, texture);
+        this(context, azAnimatorSupplier, UnaryOperator.identity(), model, texture);
     }
 
     public ProjectileRenderer(final @NonNull EntityRendererProvider.Context context, final @NonNull Supplier<AzAnimator<UUID, T>> azAnimatorSupplier, final @NonNull String id) {
         this(context, azAnimatorSupplier, JCraft.id(MODEL_STR_TEMPLATE.formatted(id)), JCraft.id(TEXTURE_STR_TEMPLATE.formatted(id)));
+    }
+
+    public ProjectileRenderer(final @NonNull EntityRendererProvider.Context context, final @NonNull String id) {
+        this(context, () -> new EntityAnimator<>(id), id);
+    }
+
+    protected ProjectileRenderer(final @NonNull AzEntityRendererConfig<T> config, final @NonNull EntityRendererProvider.Context context, final @NonNull ResourceLocation model, final @NonNull ResourceLocation texture) {
+        super(config, context, model, texture);
+    }
+
+    protected ProjectileRenderer(final @NonNull AzEntityRendererConfig<T> config, final @NonNull EntityRendererProvider.Context context, final @NonNull String id) {
+        this(config, context, JCraft.id(MODEL_STR_TEMPLATE.formatted(id)), JCraft.id(TEXTURE_STR_TEMPLATE.formatted(id)));
+    }
+
+    protected static <T extends Entity> Function<AzRendererPipelineContext<UUID, T>, AzRendererPipelineContext<UUID, T>> preRenderEntry() {
+        return pc -> {
+            final T animatable = pc.animatable();
+            final PoseStack poseStack = pc.poseStack();
+            poseStack.mulPose(Axis.YP.rotationDegrees(Mth.lerp(pc.partialTick(), animatable.yRotO, animatable.getYRot()) + 90));
+            poseStack.mulPose(Axis.ZN.rotationDegrees(Mth.lerp(pc.partialTick(), animatable.xRotO, animatable.getXRot())));
+            return pc;
+        };
     }
 
     @Override
@@ -48,10 +69,4 @@ public class ProjectileRenderer<T extends Entity> extends AbstractEntityRenderer
         return false;
     }
 
-    /*@Override
-    public void preRender(final PoseStack poseStack, final T animatable, final BakedGeoModel model, final MultiBufferSource bufferSource, final VertexConsumer buffer, final boolean isReRender, final float partialTick, final int packedLight, final int packedOverlay, final float red, final float green, final float blue, final float alpha) {
-        poseStack.mulPose(Axis.YP.rotationDegrees(Mth.lerp(partialTick, animatable.yRotO, animatable.getYRot()) + 90));
-        poseStack.mulPose(Axis.ZN.rotationDegrees(Mth.lerp(partialTick, animatable.xRotO, animatable.getXRot())));
-        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
-    }*/
 }
