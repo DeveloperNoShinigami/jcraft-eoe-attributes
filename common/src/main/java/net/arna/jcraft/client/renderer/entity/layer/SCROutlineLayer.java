33e0c65080e5b9fde7463cc83243b40d8e9fa771
Ayutac <fly.high.android@gmail.com>
2025-11-02 10:56:22 +0100

    Made SCROutlineLayer work again.

--- common/src/main/java/net/arna/jcraft/client/renderer/entity/layer/SCROutlineLayer.java
+++ common/src/main/java/net/arna/jcraft/client/renderer/entity/layer/SCROutlineLayer.java
@@ -1,13 +1,11 @@
 package net.arna.jcraft.client.renderer.entity.layer;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import mod.azure.azurelib.render.layer.AzRenderLayer;
+import lombok.NonNull;
+import mod.azure.azurelib.render.AzRendererPipelineContext;
 import net.arna.jcraft.JCraft;
 import net.arna.jcraft.common.entity.stand.ChariotRequiemEntity;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
@@ -18,15 +16,20 @@ import java.util.stream.IntStream;
 
 @Environment(EnvType.CLIENT)
 public class SCROutlineLayer extends AbstractRenderLayer<ChariotRequiemEntity> {
-    private static final List<ResourceLocation> skins = IntStream.range(0, 4).mapToObj(
+    private static final List<ResourceLocation> SKINS = IntStream.range(0, 4).mapToObj(
             i -> JCraft.id("textures/entity/stands/chariot_requiem/outline" + i + ".png")).toList();
 
-    /*@Override
-    public void render(final PoseStack poseStack, final ChariotRequiemEntity animatable, final BakedGeoModel bakedModel, final RenderType renderType,
-                       final MultiBufferSource bufferSource, final VertexConsumer buffer, final float partialTick, final int packedLight, final int packedOverlay) {
-        final RenderType cameo = RenderType.eyes(skins.get(animatable.getSkin()));
+    @Override
+    public void render(final @NonNull AzRendererPipelineContext<UUID, ChariotRequiemEntity> pc) {
+        final RenderType cameo = RenderType.eyes(SKINS.get(pc.animatable().getSkin()));
+        pc.setRenderType(cameo);
+        pc.setVertexConsumer(pc.multiBufferSource().getBuffer(cameo));
+        pc.setPackedLight(OverlayTexture.NO_OVERLAY);
+        pc.setRed(1f);
+        pc.setGreen(1f);
+        pc.setBlue(1f);
+        pc.setAlpha(1f);
+        pc.rendererPipeline().reRender(pc);
+    }
 
-        getRenderer().reRender(bakedModel, poseStack, bufferSource, animatable, cameo,
-                bufferSource.getBuffer(cameo), partialTick, packedLight, OverlayTexture.NO_OVERLAY, 1, 1, 1, 1f);
-    }*/
 }
