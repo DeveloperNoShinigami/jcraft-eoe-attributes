fe8722daaaef66e8e020e1ff3ed513baa948c402
Ayutac <fly.high.android@gmail.com>
2025-11-04 09:00:53 +0100

    Fixed render type of SHA.

--- common/src/main/java/net/arna/jcraft/client/renderer/entity/SheerHeartAttackRenderer.java
+++ common/src/main/java/net/arna/jcraft/client/renderer/entity/SheerHeartAttackRenderer.java
@@ -1,18 +1,14 @@
 package net.arna.jcraft.client.renderer.entity;
 
-import net.arna.jcraft.client.model.entity.SheerHeartAttackModel;
+import net.arna.jcraft.JCraft;
 import net.arna.jcraft.common.entity.SheerHeartAttackEntity;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.resources.ResourceLocation;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * The {@link AbstractEntityRenderer} for {@link SheerHeartAttackEntity}.
- * @see SheerHeartAttackModel
  */
 @Environment(EnvType.CLIENT)
 public class SheerHeartAttackRenderer extends AbstractEntityRenderer<SheerHeartAttackEntity> {
@@ -20,12 +16,9 @@ public class SheerHeartAttackRenderer extends AbstractEntityRenderer<SheerHeartA
     public static final String ID = "sha";
 
     public SheerHeartAttackRenderer(final EntityRendererProvider.Context context) {
-        super(context, () -> new EntityAnimator<>(ID), ID);
+        super(context, () -> new EntityAnimator<>(ID), b -> b
+                .setRenderType(RenderType.entityTranslucent(JCraft.id(TEXTURE_STR_TEMPLATE.formatted(ID)))),
+                ID);
     }
 
-    /*
-    @Override
-    public RenderType getRenderType(final SheerHeartAttackEntity animatable, final ResourceLocation texture, final @Nullable MultiBufferSource bufferSource, final float partialTick) {
-        return RenderType.entityTranslucent(getGeoModel().getTextureResource(animatable));
-    }*/
 }
