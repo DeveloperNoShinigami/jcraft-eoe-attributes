f07c644dfd14fb7487a54e3faa94488658f7ed9e
Ayutac <fly.high.android@gmail.com>
2025-11-04 08:16:29 +0100

    Fixed render type of ankh.

--- common/src/main/java/net/arna/jcraft/client/renderer/entity/projectiles/AnkhRenderer.java
+++ common/src/main/java/net/arna/jcraft/client/renderer/entity/projectiles/AnkhRenderer.java
@@ -1,16 +1,13 @@
 package net.arna.jcraft.client.renderer.entity.projectiles;
 
 import lombok.NonNull;
-import net.arna.jcraft.client.model.JProjectileModel;
+import net.arna.jcraft.JCraft;
 import net.arna.jcraft.common.entity.projectile.AnkhProjectile;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceLocation;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * The {@link ProjectileRenderer} for {@link AnkhProjectile}.
@@ -18,8 +15,12 @@ import org.jetbrains.annotations.Nullable;
 @Environment(EnvType.CLIENT)
 public class AnkhRenderer extends ProjectileRenderer<AnkhProjectile> {
 
+    public static final String ID = "ankh";
+
     public AnkhRenderer(final @NonNull EntityRendererProvider.Context context) {
-        super(context, "ankh");
+        super(context, () -> new EntityAnimator<>(ID), b -> b
+                .setRenderType(RenderType.eyes(JCraft.id(TEXTURE_STR_TEMPLATE.formatted(ID)))),
+                ID);
     }
 
     @Override
@@ -27,9 +28,4 @@ public class AnkhRenderer extends ProjectileRenderer<AnkhProjectile> {
         return 15;
     }
 
-    /*@Override
-    public RenderType getRenderType(final @NonNull AnkhProjectile animatable, final ResourceLocation texture,
-                                    final @Nullable MultiBufferSource bufferSource, final float partialTick) {
-        return RenderType.eyes(texture);
-    }*/
 }
