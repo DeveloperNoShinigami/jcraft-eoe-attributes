e06bdd46f089361c2b7e6d3716f8d09ac21a025b
Ayutac <fly.high.android@gmail.com>
2025-11-04 15:47:52 +0100

    Removed SandTornadoModel.

--- common/src/main/java/net/arna/jcraft/client/renderer/entity/SandTornadoRenderer.java
+++ common/src/main/java/net/arna/jcraft/client/renderer/entity/SandTornadoRenderer.java
@@ -1,15 +1,10 @@
 package net.arna.jcraft.client.renderer.entity;
 
 import lombok.NonNull;
-import net.arna.jcraft.client.model.entity.SandTornadoModel;
 import net.arna.jcraft.common.entity.projectile.SandTornadoEntity;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.resources.ResourceLocation;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * The {@link AbstractEntityRenderer} for {@link SandTornadoEntity}.
