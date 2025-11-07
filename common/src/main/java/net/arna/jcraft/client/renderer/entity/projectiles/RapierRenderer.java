0b942c9ddb14d70e6fe1836df30e17b5513ef9ae
Ayutac <fly.high.android@gmail.com>
2025-11-07 04:07:59 +0100

    Fixed Rapier texture.

--- common/src/main/java/net/arna/jcraft/client/renderer/entity/projectiles/RapierRenderer.java
+++ common/src/main/java/net/arna/jcraft/client/renderer/entity/projectiles/RapierRenderer.java
@@ -1,25 +1,51 @@
 package net.arna.jcraft.client.renderer.entity.projectiles;
 
 import lombok.NonNull;
+import mod.azure.azurelib.render.entity.AzEntityRendererConfig;
+import mod.azure.azurelib.render.entity.AzEntityRendererPipeline;
 import net.arna.jcraft.JCraft;
-import net.arna.jcraft.client.model.entity.RapierModel;
 import net.arna.jcraft.common.entity.projectile.RapierProjectile;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * The {@link ProjectileRenderer} for {@link RapierProjectile}.
- * @see RapierModel
  */
 public class RapierRenderer extends ProjectileRenderer<RapierProjectile> {
+    protected static final List<ResourceLocation> SKINS = IntStream.range(0, 4).mapToObj(
+            i -> JCraft.id("textures/entity/stands/silver_chariot/rapier_" + (i == 0 ? "default" : "skin" + i) + ".png")).toList();
 
     public static final String ID = "rapier";
 
-    // TODO fix skins
     public RapierRenderer(final @NonNull EntityRendererProvider.Context context) {
-        super(context, () -> new EntityAnimator<>("animations/knife.animation.json"), b -> b
-                .setRenderType(RenderType.entityTranslucent(JCraft.id(TEXTURE_STR_TEMPLATE.formatted(ID)))),
-                ID);
+        super(AzEntityRendererConfig.builder(
+                entity -> JCraft.id(MODEL_STR_TEMPLATE.formatted(ID)),
+                RapierRenderer::getTexture
+                )
+                .setRenderType(entity -> RenderType.entityTranslucent(getTexture(entity)))
+                .setAnimatorProvider(() -> new EntityAnimator<>(ID))
+                .setModelRenderer((pipeline, layer) ->
+                        new ProjectileModelRenderer<>((AzEntityRendererPipeline<RapierProjectile>) pipeline, layer))
+                .build(),
+                context, ID);
+    }
+
+    @Override
+    public @NonNull ResourceLocation getTextureLocation(final @NonNull RapierProjectile rapier) {
+        return getTexture(rapier);
+    }
+
+    protected static ResourceLocation getTexture(final @NonNull RapierProjectile rapier) {
+        final int skin = rapier.getSkin();
+        return switch (skin) {
+            case -2 -> RapierProjectile.POSSESSED_TEXTURE;
+            case -1 -> RapierProjectile.ARMOR_OFF_TEXTURE;
+            default -> SKINS.get(skin);
+        };
     }
 
 }
