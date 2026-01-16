97f53d9935b1122d8237af5c22bd85f979363f62
Ayutac <fly.high.android@gmail.com>
2026-01-16 22:21:04 +0100

    Fixed hamon aura potentially being visible in spectator.

--- common/src/main/java/net/arna/jcraft/client/renderer/features/HamonParticlesFeatureRenderer.java
+++ common/src/main/java/net/arna/jcraft/client/renderer/features/HamonParticlesFeatureRenderer.java
@@ -20,6 +20,7 @@ import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
@@ -65,7 +66,9 @@ public class HamonParticlesFeatureRenderer<T extends LivingEntity, M extends Age
         lastTickCounts.put(id, livingEntity.tickCount);
 
         final int m = getHamonCharge(livingEntity);
-        if (m <= 0) return;
+        if (m <= 0 || livingEntity.isSpectator()) {
+            return;
+        }
 
         final RandomSource random = livingEntity.getRandom();
         final AnimalModelAccessor accessor = (AnimalModelAccessor) getParentModel();
