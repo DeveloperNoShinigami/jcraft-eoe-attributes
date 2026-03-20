3439f2ff2f3f1e6accf2b1f3f97f86732acdc9fe
DeveloperNoShinigami <bluelotuscoding@gmail.com>
2026-03-20 14:46:16 -0500

    Reset project to blank Architectury template for 1.20.1 BlueLotusCoding version

---
+++ common/src/main/java/com/bluelotuscoding/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.bluelotuscoding;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "jcraft_eoe_attributes";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
